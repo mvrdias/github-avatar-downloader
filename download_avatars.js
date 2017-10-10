e2eca694a8276681ebc4786bb46e70e85f146f0c
Marcus Dias <mvrdias@uol.com.br>
2017-10-10 15:55:51 +0000

    com comentarios de fotos

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = "mvrdias";
 var GITHUB_TOKEN = "a74fa7a0900348d02b5d7e1c1762515ff30c0f78";
@@ -11,9 +12,16 @@ const options = {
   headers: {
     "User-Agent": GITHUB_USER
   }
-}
+};
 
 console.log (requestURL);
+
+//  function downloadImageByURL(url, filePath) {
+
+//   request.get(url)
+//        .pipe(fs.createWriteStream(filePath));
+
+//  }
 
 request(options, function( err, response, body) {
     var contList = JSON.parse(body);
@@ -21,8 +29,24 @@ request(options, function( err, response, body) {
     console.log(contList);
     contList.forEach(function(element) {
        console.log('each', element.avatar_url, element.login);
+    // downloadImageByURL( element.avatar_url,
+//                        'avatar/' + element.login + '.jpg')
     })
 });
 
 
 
+
+//  request(options,
+//     function(err, response, body){
+//         var contList = JSON.parse(body);
+//         console.log(contList);
+//         contList.forEach(function(element) {
+//             console.log('each',element.avatar_url,
+//                 element.login);
+//             downloadImageByURL(
+//                 element.avatar_url,
+//                 'avatar/' + element.login + '.jpg'
+//             )
+//             })
+//     });
