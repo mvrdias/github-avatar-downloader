31c9eb2057a91917b3d79b8a7740f4638ebde65e
Marcus Dias <mvrdias@uol.com.br>
2017-10-11 02:08:14 +0000

    Step 10 - Final Revision

--- download_avatars.js
+++ download_avatars.js
@@ -4,44 +4,52 @@
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
-// Var , Const and Personal Informations
-var request = require('request');
-var fs = require('fs');
+// test: console.log(`Repo Owner: '${repoOwner}', RepoName: '${repoName}'`);
 
-var GITHUB_USER = "mvrdias";
-var GITHUB_TOKEN = "a74fa7a0900348d02b5d7e1c1762515ff30c0f78";
+if (repoOwner && repoName) {
 
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
-                 '@api.github.com/repos/' + repoOwner + '/' +
-                 repoName + '/contributors';
 
-const options = {
-  url:
-    requestURL,
-  headers: {
-    "User-Agent": GITHUB_USER
+  // Var , Const and Personal Informations
+  var request = require('request');
+  var fs = require('fs');
+
+  var GITHUB_USER = "mvrdias";
+  var GITHUB_TOKEN = "a74fa7a0900348d02b5d7e1c1762515ff30c0f78";
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
+                   '@api.github.com/repos/' + repoOwner + '/' +
+                   repoName + '/contributors';
+
+  const options = {
+    url:
+      requestURL,
+    headers: {
+      "User-Agent": GITHUB_USER
+    }
+  };
+
+
+  // Check .. if it is working well
+  console.log (requestURL);
+
+  // Download Images
+  function downloadImageByURL(url, filePath) {
+
+     request.get(url).pipe(fs.createWriteStream(filePath));
   }
-};
 
+  // Request Infomations
+  request(options, function( err, resp, body) {
+      var contList = JSON.parse(body);
+      //console.log(body);
+      //console.log(contList);
 
-// Check .. if it is working well
-console.log (requestURL);
-
-// Download Images
-function downloadImageByURL(url, filePath) {
-
-   request.get(url).pipe(fs.createWriteStream(filePath));
+      contList.forEach(function(elem) {
+         console.log('each', elem.avatar_url, elem.login);
+         downloadImageByURL( elem.avatar_url,
+                            'avatar/' + elem.login + '.jpg')
+      })
+  });
+} else {
+  console.log ("Please, re-enter with corrects datas: download_avatars.js <owner> <repo>");
 }
-
-// Request Infomations
-request(options, function( err, resp, body) {
-    var contList = JSON.parse(body);
-    //console.log(body);
-    //console.log(contList);
-
-    contList.forEach(function(elem) {
-       console.log('each', elem.avatar_url, elem.login);
-       downloadImageByURL( elem.avatar_url,
-                          'avatar/' + elem.login + '.jpg')
-    })
-});
